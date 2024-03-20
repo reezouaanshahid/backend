e341998bd854b9abe43a6f9c1219a77f07d0f493
rizwan <rizwanshahed232@gmail.com>
2024-03-20 19:20:28 +0500

    import data by url

--- connection.js
+++ connection.js
@@ -13,7 +13,7 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     
-    fs.appendFile('mynewfile12.txt', 'Hello content!', function (err) {
+    fs.appendFile('v', 'Hello content!', function (err) {
         if (err) throw err;
         console.log('Saved!');
       });
